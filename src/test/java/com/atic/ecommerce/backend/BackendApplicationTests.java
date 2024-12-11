51b7a640afcbd7d1862ab9cff3f1da7431b39beb
Aletdev <alejandrortsinani@outlook.com>
2024-12-11 17:06:52 -0400

    Commit inicial scaffolding y creacion de archivos de configuracion

---
+++ src/test/java/com/atic/ecommerce/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atic.ecommerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
